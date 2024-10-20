302e64dc893bee29a56a70220a31d48954aaa0f6
svichkar olga <syns41@gmail.com>
2024-10-21 00:20:48 +0300

    small fixes

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styles from "./NotFoundPage.module.css";
 
 const NotFoundPage = () => {
   return (
-    <div>
-      No pages was found. Please, go to the <Link to="/">home page</Link>
+    <div className={styles.box}>
+      No pages was found. Please, go to the
+      <Link to="/">
+        <button className={styles.btn}>Home page</button>
+      </Link>
     </div>
   );
 };
